3ac7b7ff4f3deab109e72873d21eb65e08c9e32c
Pedro Santiago <pedrossantiago1@gmail.com>
2022-06-01 18:40:49 -0300

    adc tabela

--- Parte 1/index.js
+++ Parte 1/index.js
@@ -1,3 +1,25 @@
-let produtos = {}
+let produtos = [
+	{ id: '1', nome: 'Produto 1', marca: 'Marca 1', preco: 10.4, qtd: 1 },
+	{ id: '2', nome: 'Produto 2', marca: 'Marca 2', preco: 20, qtd: 2 },
+	{ id: '3', nome: 'Produto 3', marca: 'Marca 3', preco: 30, qtd: 3 },
+	{ id: '4', nome: 'Produto 4', marca: 'Marca 4', preco: 40, qtd: 4 },
+	{ id: '5', nome: 'Produto 5', marca: 'Marca 5', preco: 50, qtd: 5 }
+]
 
 let logs = []
+
+function attTabela() {
+	let tabela = document.getElementById('corpoTabela')
+	for (let i = 0; i < produtos.length; i++) {
+		let produto = produtos[i]
+		tabela.innerHTML += `
+        <tr>
+            <td>${produto.id}</td>
+            <td>${produto.nome}</td>
+            <td>${produto.marca}</td>
+            <td>R$ ${produto.preco}</td>
+            <td>${produto.qtd}</td>
+        </tr>
+        `
+	}
+}
