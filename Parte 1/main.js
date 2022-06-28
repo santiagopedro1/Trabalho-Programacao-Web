8ab23e77d250559e63921ed15ed252f0b02df256
Pedro Santiago <pedrossantiago1@gmail.com>
2022-06-28 19:22:00 -0300

    .

--- Parte 1/main.js
+++ Parte 1/main.js
@@ -14,6 +14,7 @@ if (!window.localStorage.getItem('produtos')) {
 			'Adicionado: ID: 1 - Notebook - Marca: Dell - R$ 2000 - 10 itens em 01/01/2020 00:00:00',
 			'Adicionado: ID: 2 - Mouse - Marca: Logitech - R$ 50 - 20 itens em 01/01/2020 00:00:00',
 			'Adicionado: ID: 3 - Teclado - Marca: Logitech - R$ 100 - 30 itens em 01/01/2020 00:00:00',
+			'Adicionado: ID: 4 - Headphones - Marca: Logitech - R$ 90 - 40 itens em 01/01/2020 00:00:00',
 			'Removido: 40 unidades do produto ID: 4 - Headphones - Marca: Logitech em 01/01/2020 00:00:00'
 		])
 	)
