0d88bf39854ae458be176e07fed1253f7792c442
Pedro Santiago <pedrossantiago1@gmail.com>
2022-06-28 18:09:21 -0300

    .

--- Parte 1/main.js
+++ Parte 1/main.js
@@ -11,10 +11,11 @@ if (!window.localStorage.getItem('produtos')) {
 	window.localStorage.setItem(
 		'logs',
 		JSON.stringify([
-		'Adicionado: ID: 1 - Notebook - Marca: Dell - R$ 2000 - 10 itens em 01/01/2020 00:00:00',
-		'Adicionado: ID: 2 - Mouse - Marca: Logitech - R$ 50 - 20 itens em 01/01/2020 00:00:00',
-		'Adicionado: ID: 3 - Teclado - Marca: Logitech - R$ 100 - 30 itens em 01/01/2020 00:00:00'
-	])
+			'Adicionado: ID: 1 - Notebook - Marca: Dell - R$ 2000 - 10 itens em 01/01/2020 00:00:00',
+			'Adicionado: ID: 2 - Mouse - Marca: Logitech - R$ 50 - 20 itens em 01/01/2020 00:00:00',
+			'Adicionado: ID: 3 - Teclado - Marca: Logitech - R$ 100 - 30 itens em 01/01/2020 00:00:00',
+			'Removido: 40 unidades do produto ID: 4 - Headphones - Marca: Logitech em 01/01/2020 00:00:00'
+		])
 	)
 }
 let produtos = JSON.parse(window.localStorage.getItem('produtos'))
