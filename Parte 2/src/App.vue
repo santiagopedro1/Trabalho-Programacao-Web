5fe583bec34647d21e148a14fbf937b2ab6cd515
Pedro Santiago <pedrossantiago1@gmail.com>
2022-06-30 02:15:18 -0300

    Ínicio da parte 2 do trabalho

---
+++ Parte 2/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <HelloWorld />
+</template>
+
+<style>
+body {
+    box-sizing: border-box;
+    line-height: 1.4;
+}
+#app {
+    background-color: #dcdcdc;
+}
+</style>
